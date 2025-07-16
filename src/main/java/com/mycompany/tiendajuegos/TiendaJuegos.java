08d935dac50089a2181a8bb51c3ebd8943a45c0f
Fercho2503 <fcf.cabrera25@gmail.com>
2025-07-16 16:16:24 -0600

    Creacion de clases y respectivos atributos

--- src/main/java/com/mycompany/tiendajuegos/TiendaJuegos.java
+++ src/main/java/com/mycompany/tiendajuegos/TiendaJuegos.java
@@ -4,6 +4,7 @@
 
 package com.mycompany.tiendajuegos;
 import javax.swing.JOptionPane;
+import javax.swing.ImageIcon;
 
 /**
  *
@@ -14,10 +15,13 @@ public class TiendaJuegos {
     public static void main(String[] args) {
         //System.out.println("Hello World!");
         
-        JOptionPane.showMessageDialog(null, "Bienvenido a la tienda de Videojuegos Apache");
+        ImageIcon marca = new ImageIcon("src/logo.jpg");
+        
+        JOptionPane.showMessageDialog(null, "BIENVENIDO A NUESTRA TIENDA DE VIDEOJUEGOS", "TIENDAS APACHE", 
+                JOptionPane.INFORMATION_MESSAGE, marca);
         JOptionPane.showInputDialog(null, "Que desea hacer el dia de hoy?\n1-Clientes\n2-Videojuegos\n"
-                + "3-Ventas\n4-Intercambios\n5-Salir del sistema\n"
-                + "Prueba Push Saul 2");
+                + "3-Ventas\n4-Intercambios\n5-Salir del sistema");
+        
         
         
         
