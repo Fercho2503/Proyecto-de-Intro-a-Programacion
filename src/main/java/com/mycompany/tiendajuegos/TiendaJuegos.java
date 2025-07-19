f0f232ab94a5eebb37866756d3813e59b3d7aa68
saulc <saulc@DESKTOP-MM8DK7J>
2025-07-18 22:30:06 -0600

    Prueba Push Saul

--- src/main/java/com/mycompany/tiendajuegos/TiendaJuegos.java
+++ src/main/java/com/mycompany/tiendajuegos/TiendaJuegos.java
@@ -19,8 +19,24 @@ public class TiendaJuegos {
         
         JOptionPane.showMessageDialog(null, "BIENVENIDO A NUESTRA TIENDA DE VIDEOJUEGOS", "TIENDAS APACHE", 
                 JOptionPane.INFORMATION_MESSAGE, marca);
-        JOptionPane.showInputDialog(null, "Que desea hacer el dia de hoy?\n1-Clientes\n2-Videojuegos\n"
-                + "3-Ventas\n4-Intercambios\n5-Salir del sistema");
+        JOptionPane.showInputDialog(null, "****MENÚ****\n1-Clientes\n2-Productos\n3-Ventas\n4-Intercambios\n5-Salir del sistema");
+        
+        //Menu Clientes
+        JOptionPane.showInputDialog(null, "CLIENTES\n1- Registrar cliente\n2-Modificar clientes\n3-Eliminar Cliente\n\n4-Volver al menu principal");
+        
+        //Menu PRODUCTOS
+        JOptionPane.showInputDialog(null, "****PRODCUTOS****\n1-Registrar producto\n2-Consultar productos\n\n3-Volver al menu principal");
+        
+        
+        //Menu Ventas
+        JOptionPane.showInputDialog(null, "****VENTAS****\n1-Realizar venta\n\n2-Volver al menu principal");
+        
+        //Menu Intercambios
+        JOptionPane.showInputDialog(null, "****INTERCAMBIOS****\n1-Lista de juegos\n2-Registro juego cliente\n3-Generar comprobante intercambio\n\n4-Volver al menu principal");
+
+
+        
+        
         
         
         
